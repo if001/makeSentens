40f02c6d302896e04e3ec431ec2942c9493b7b51
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2017-10-24 10:18:11 +0900

    修正

--- rm_bigfile.sh
+++ rm_bigfile.sh
@@ -10,4 +10,4 @@ TARGETS=(
 target=$(printf " %s" "${TARGETS[@]}")
 target=${target:1}
 
-git filter-branch --index-filter "git rm -r --cached --ignore-unmatch ${target}" -- --all
+git filter-branch -f --index-filter "git rm -r --cached --ignore-unmatch ${target}" -- --all
