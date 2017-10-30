33d910bc3407db308b558836f732e384ad9566a7
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2017-10-30 12:56:17 +0900

    出力層に全結合を追加 学習法を明示的に表記

--- nn/Seq2Seq.py
+++ nn/Seq2Seq.py
@@ -12,34 +12,43 @@ from keras.optimizers import Adam
 
 from keras.layers.wrappers import Bidirectional as Bi
 from keras.layers.wrappers import TimeDistributed as TD
-from keras.layers          import Lambda, Input, Dense, GRU, LSTM, RepeatVector
+from keras.layers          import Lambda, Input, Dense, GRU, LSTM, RepeatVector, concatenate
 from keras.models          import Model
 from keras.layers.core     import Flatten
 from keras.layers          import merge, multiply
 
+from keras.callbacks import EarlyStopping
 
+import sys,os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # mylib
 import lib
 
+
 class Seq2Seq(lib.Const.Const):
-    def __init__(self):
+    def __init__(self,encord_len,decord_len):
         super().__init__()
         self.input_word_num = 1
-        self.hidden_dim = 7000
+        # self.hidden_dim = 7000
+        self.encord_len = encord_len
+        self.decord_len = decord_len
 
     def make_net(self):
-        # ---------------
-        latent_dim = 1000
-        inputs = Input(shape=(self.seq_num, self.word_feat_len))
-        encoded = LSTM(latent_dim)(inputs)
-        decoded = RepeatVector(self.seq_num)(encoded)
-        decoded = LSTM(self.word_feat_len, return_sequences=True)(decoded)
+        # ---------------        
+        latent_dim = 500
+        input_dim = self.word_feat_len
+        inputs = Input(shape=(self.encord_len, input_dim))
+        encoded = LSTM(latent_dim,activation="tanh",recurrent_activation="sigmoid",return_sequences=False)(inputs)
+                       
+        decoded = RepeatVector(self.decord_len)(encoded)
+        decoded = LSTM(input_dim, activation="tanh",recurrent_activation="sigmoid",return_sequences=True)(decoded)
+        decoded = Dense(input_dim, activation="linear")(decoded)
 
         self.sequence_autoencoder = Model(inputs, decoded)
-        # encoder = Model(inputs, encoded)
+        self.sequence_autoencoder.compile(optimizer='rmsprop', loss='mse')
         # ---------------
 
-
+        
 
         # ---- modelで組む ------
         # model=Sequential()
@@ -84,27 +93,30 @@ class Seq2Seq(lib.Const.Const):
         # ---------------
 
 
-        self.sequence_autoencoder = Model(inputs, decoded)
 
+        # self.sequence_autoencoder = Model(inputs, decoded)
 
-
-        loss = 'mean_squared_error'
-        optimizer = 'rmsprop'
-        optimizer = 'Adam'
-        # self.model.compile(loss=loss, optimizer=optimizer)
-        # self.model.summary()
-        self.sequence_autoencoder.compile(loss=loss, optimizer=optimizer)
+        # loss = 'mean_squared_error'
+        # optimizer = 'rmsprop'
+        # optimizer = 'Adam'
+        # # self.model.compile(loss=loss, optimizer=optimizer)
+        # # self.model.summary()
+        # self.sequence_autoencoder.compile(loss=loss, optimizer=optimizer)
         self.sequence_autoencoder.summary()
 
 
 
     def train(self,X_train,Y_train):
+
+        es_cb = EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
         self.sequence_autoencoder.fit(X_train, Y_train,
-                       shuffle=True,
-                       nb_epoch=5,
-                       batch_size=self.batch_size,
-                       validation_split=0.1,
-                       verbose=1)
+                                      shuffle=True,
+                                      #nb_epoch=9,
+                                      nb_epoch=30,
+                                      batch_size=self.batch_size,
+                                      validation_split=0.1,
+                                      verbose=1,
+                                      callbacks=[es_cb])
 
         # self.model.fit(X_train, Y_train,
         #                shuffle=True,
@@ -118,55 +130,54 @@ class Seq2Seq(lib.Const.Const):
         predict_list = self.sequence_autoencoder.predict_on_batch(inp)
         return predict_list
 
-    def waitController(self,flag):
+    def waitController(self,flag,fname):
         try:
             if flag == "save":
-                print("save")
-                self.sequence_autoencoder.save_weights(self.seq2seq_wait_save_dir)
-
+                print("save"+self.seq2seq_wait_save_dir+fname)
+                self.sequence_autoencoder.save_weights(self.seq2seq_wait_save_dir+fname)
             if flag == "load":
-                print("load")
-                self.sequence_autoencoder.load_weights(self.seq2seq_wait_save_dir)
+                print("load"+self.seq2seq_wait_save_dir+fname)
+                self.sequence_autoencoder.load_weights(self.seq2seq_wait_save_dir+fname)
         except :
-            print(self.seq2seq_wait_save_dir+" dose not exist")
+            print(self.seq2seq_wait_save_dir+fname+" dose not exist")
             sys.exit(0)
 
 
 
 def main():
-    seq2seq = Seq2Seq()
+    seq2seq = Seq2Seq(5,5)
     seq2seq.make_net()
 
-    word = np.array([1,2,3])
-    word2 = np.array([2,3,4])
 
-    # one of train
-    input_vec = []
-    input_vec.append(word)
-    input_vec.append(word2)
-    input_vec = np.array(input_vec)
+    import random
 
-    # one of train
-    input_vec2 = []
-    input_vec2.append(word)
-    input_vec2.append(word2)
-    input_vec2 = np.array(input_vec)
+    inp_batch = []
+    out_batch = []
+    for value in range(seq2seq.batch_size):
+        i = random.randint(0,10)/10
+        inp = [[i,i+0.1],[i+0.2,i+0.3],[i+0.4,i+0.5],[i+0.6,i+0.7],[i+0.8,i+0.9]]
+        inp_batch.append(inp)
+        out = [[i+1.0,i+1.1],[i+1.2,i+1.3],[i+1.4,i+1.5],[i+1.6,i+1.7],[i+1.8,i+1.9]]
+        out_batch.append(out)
 
-    # batch
-    batch_input_vec = []
-    batch_input_vec.append(input_vec)
-    batch_input_vec.append(input_vec2)
-    batch_input_vec = np.array(batch_input_vec)
-    print(batch_input_vec.shape)
+    inp_batch = np.array(inp_batch)
+    out_batch = np.array(out_batch)
+    for i in range(50):
+        seq2seq.train(inp_batch,out_batch)
 
-    # train
-    seq2seq.train(batch_input_vec,batch_input_vec)
 
-    # test1
-    test_input = np.array([input_vec])
-    print(test_input)
-    predict = seq2seq.predict(test_input)
-    print(predict)
+    for value in range(10):
+        inp_batch = []
+        i = random.randint(0,10)/10
+        inp = [[i,i+0.1],[i+0.2,i+0.3],[i+0.4,i+0.5],[i+0.6,i+0.7],[i+0.8,i+0.9]]
+        inp_batch.append(inp)
+        inp_batch = np.array(inp_batch)
+
+        
+        predict = seq2seq.predict(inp_batch)
+        print("inp :",inp_batch)
+        print("test:",predict)
+
 
 
 if __name__ == "__main__":
