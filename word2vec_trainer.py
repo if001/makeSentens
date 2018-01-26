455531d821b599ec208acab0b6b735b56e8babcb
IsseiFUJIMOTO <otomijuf.004@gmail.com>
2018-01-26 17:46:04 +0900

    確認コード

--- word2vec_trainer.py
+++ word2vec_trainer.py
@@ -9,4 +9,10 @@ model = wv.MyWord2Vec().load_model()
 print("corpus: ", model.corpus_count)
 voc = model.wv.vocab.keys()
 print("vocab: ", len(voc))
+print(voc)
 
+# vec = wv.MyWord2Vec().str_to_vector(model, "引か")
+# print(vec)
+# import pylab as plt
+# plt.plot(vec)
+# plt.show()
